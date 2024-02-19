a4076e2fd92b840dec0dc0160dba42ba8c13f6c2
Luiz Smania <luizsmania1@gmail.com>
2024-02-19 20:12:39 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OlcxKEm46oXQWMaCrBxN4XKH5O5IlSnThHNJ55chTCke8lC4ylU8l2MlU3JkaaEtXIlyN9JPxKNk7c5uSVWyrBo00XxoEUoqR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
