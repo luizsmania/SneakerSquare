d290d943f1b563ddf59fd6eafac0eb5526fc9fa0
Luiz Smania <luizsmania1@gmail.com>
2024-02-25 00:43:43 +0000

    Added privacy webpage

--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def index(request):
     """" A view to return the index pages """
     return render(request, 'home/index.html')
+
+
+def privacy(request):
+    return render(request, 'home/privacy.html')
